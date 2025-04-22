5ab1c53ca95ec3b28ab527c8df9f738630538127
Arjun <arjunbishnoi1@gmail.com>
2025-04-22 03:16:17 -0400

    Update Next.js configuration for dynamic base path and asset prefix based on environment, enhance deployment script to specify branch for GitHub Pages, and add new file for output structure.

--- next.config.js
+++ next.config.js
@@ -6,8 +6,8 @@ const nextConfig = {
     unoptimized: true
   },
   trailingSlash: true,
-  basePath: '/singh.co',
-  assetPrefix: '/singh.co/',
+  basePath: process.env.NODE_ENV === 'production' ? '/singh.co' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/singh.co/' : '',
 };
 
 module.exports = nextConfig;
